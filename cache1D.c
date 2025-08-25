93089ed563eec1cca0e304a52cc86371d85fb94e
Sai Gungurthi <98351945+gungur@users.noreply.github.com>
2025-08-24 20:09:19 -0500

    Add files via upload

---
+++ cache1D.c
@@ -0,0 +1,11 @@
+/**
+* Sai Gungurthi
+*/
+
+int arr[100000];
+
+int main() {
+	for (int i = 0; i < 100000; i++) {
+		arr[i] = i;
+	}
+}
---
+++ cache2Dclash.c
@@ -0,0 +1,15 @@
+/**
+* Sai Gungurthi
+*/
+
+int arr2D[128][8];
+
+int main() {
+	for (int iteration = 0; iteration < 100; iteration++) {
+		for (int row = 0; row < 128; row += 64) {
+			for (int col = 0; col < 8; col++) {
+				arr2D[row][col] = iteration + row + col;
+			}
+		}
+	}
+}
